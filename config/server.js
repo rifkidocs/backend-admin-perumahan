f68ff7a8d65e633625adcf2c088dd8ca5604b455
rifkidocs <rifkiars@gmail.com>
2025-10-01 06:09:30 +0700

    feat: add commented URL configuration in server settings
    This commit adds a commented-out URL configuration in the server settings file, allowing for easy customization of the backend URL without affecting the current functionality.
    
    Signed-off-by: rifkidocs <rifkiars@gmail.com>

--- config/server.js
+++ config/server.js
@@ -1,5 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
+  // url: ("https://backend-perumahan.rifkidocs.eu.org"),
   port: env.int('PORT', 1340),
   app: {
     keys: env.array('APP_KEYS'),
